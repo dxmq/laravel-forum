b51ab7a73e290af902cc13abc970e74a41cfc234
h1ml <1697859639@qq.com>
2019-05-04 21:16:06 +0800

    add favorite feature to reply

--- app/Reply.php
+++ app/Reply.php
@@ -16,4 +16,23 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if (! $this->favorites()->where($attributes)->exists()) { // 用户不能重复点赞
+            return $this->favorites()->create($attributes);
+        }
+    }
+
+    public function isFavorited()
+    {
+        return $this->favorites()->where(['user_id' => auth()->id()])->exists();
+    }
 }
